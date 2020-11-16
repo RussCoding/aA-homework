de47ade30f256fb7d63d8391d32c8668575efca4
russelkamal <russelhkamal@gmail.com>
2020-11-15 22:15:19 -0500

    finished

--- W4D1/graph_nodes.rb
+++ W4D1/graph_nodes.rb
@@ -1,3 +1,4 @@
+require "set"
 class GraphNode
     attr_accessor :val, :neighbors
 
@@ -6,17 +7,31 @@ class GraphNode
         @neighbors = []
     end
 
-    def bfs(starting_node, target_value)
-        return starting_node if starting_node.val == target_value
-        visited = [starting_node]
-        queue = starting_node.neighbors.map {|node| node}
-        while !queue.empty?
-            current = queue.shift
-            return current if current.val == target_value
-            visited << current
-            current.neighbors.each {|node| queue << node if !visited.include?(node)}
-        end
-        nil
-
-    end
+    
 end
+def bfs(starting_node, target_value)
+    return starting_node if starting_node.val == target_value
+    visited = Set[starting_node.val]
+    queue = starting_node.neighbors.map {|node| node}
+    while !queue.empty?
+        current = queue.shift
+        return current if current.val == target_value
+        visited.add(current.val)
+        current.neighbors.each {|node| queue << node if !visited.include?(node.val)}
+    end
+
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+p bfs(a, "b")
+p bfs(a, "f")
+
